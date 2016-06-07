df34b609d675e4c6d1ed690b58ae32e3e71ed211
Arun Pandi <arunpandieie@gmail.com>
2016-06-07 14:19:43 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]==i)
+            printf("%d",a[i]);
+    }
+    return 0;
+}
